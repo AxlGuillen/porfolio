ceceb1627d51b73fbd36d05811ad5a1480fe3802
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-05-19 20:50:07 -0600

    Tailwind animated instalado y listo

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,5 +1,11 @@
 <template>
   <h1 className="text-4xl font-bold text-red-600">Hello world!</h1>
+
+  <button class="animate-wiggle">¡Mírame moverme!</button>
+
+  <button class="animate-jump-in animate-delay-300 animate-once">
+    Espera un poco, luego entra saltando.
+  </button>
 </template>
 
 <script>
