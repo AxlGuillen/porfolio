c0632713b93b7edd867888af02fbee33d831ac5f
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-06-16 21:38:38 -0600

    Iconos listo

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -64,35 +64,50 @@
             <font-awesome-icon :icon="['fab', 'linkedin']" />
           </a>
         </div>
-        <div class="flex flex-row gap-3">
-          <img src="../assets/iconos/python.png" alt="Python" class="h-6 w-6" />
-          <img src="../assets/iconos/js.png" alt="JavaScript" class="h-6 w-6" />
-          <img src="../assets/iconos/vue.png" alt="Vue" class="h-6 w-6" />
-          <img
-            src="../assets/iconos/laravel.png"
-            alt="Laravel"
-            class="h-6 w-6"
-          />
-          <img
-            src="../assets/iconos/tailwind.png"
-            alt="Tailwind"
-            class="h-6 w-6"
-          />
-          <img
-            src="../assets/iconos/flutter.png"
-            alt="Flutter"
-            class="h-6 w-6"
-          />
-          <img
-            src="../assets/iconos/firebase.png"
-            alt="Firebase"
-            class="h-6 w-6"
-          />
-          <img
-            src="../assets/iconos/postgres.png"
-            alt="Postgres "
-            class="h-6 w-6"
-          />
+        <div
+          class="flex flex-col gap-3 mt-2 animate-fade-up animate-once animate-delay-[3300ms] animate-ease-in"
+        >
+          <div>
+            <h1 class="text-sm">Skills</h1>
+          </div>
+          <div class="flex flex-row gap-3">
+            <img
+              src="../assets/iconos/python.png"
+              alt="Python"
+              class="h-6 w-6"
+            />
+            <img
+              src="../assets/iconos/js.png"
+              alt="JavaScript"
+              class="h-6 w-6"
+            />
+            <img src="../assets/iconos/vue.png" alt="Vue" class="h-6 w-6" />
+            <img
+              src="../assets/iconos/laravel.png"
+              alt="Laravel"
+              class="h-6 w-6"
+            />
+            <img
+              src="../assets/iconos/tailwind2.png"
+              alt="Tailwind"
+              class="h-6 w-6"
+            />
+            <img
+              src="../assets/iconos/flutter.png"
+              alt="Flutter"
+              class="h-6 w-6"
+            />
+            <img
+              src="../assets/iconos/postgres.png"
+              alt="Postgres "
+              class="h-6 w-6"
+            />
+            <img
+              src="../assets/iconos/firebase.png"
+              alt="Firebase"
+              class="h-6 w-6"
+            />
+          </div>
         </div>
       </div>
       <div
