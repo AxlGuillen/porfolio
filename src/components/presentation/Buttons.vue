6247b9bbca11763d2a00b837cdf73e74ed6098d1
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-08-06 14:19:34 -0600

    Flowbite instalado y configurado. Dependencia de fontawesome desintaladas y quitadas

--- src/components/presentation/Buttons.vue
+++ src/components/presentation/Buttons.vue
@@ -7,7 +7,7 @@
       download="Axl_Guillen_CV.pdf"
       class="rounded p-1 border-b-4 border-blue-600 hover:bg-blue-600 hover:border-b-0 flex items-center space-x-2"
     >
-      <font-awesome-icon :icon="['fas', 'file-arrow-down']" />
+      <Download class="text-white"/>
       <span class="text-sm">{{ t("presentation.cv") }}</span>
     </a>
     <a
@@ -15,19 +15,24 @@
       target="_blank"
       class="border-b-4 rounded p-1 border-blue-600 hover:bg-blue-600 hover:border-b-0 flex items-center justify-center w-10 h-10"
     >
-      <font-awesome-icon :icon="['fab', 'github']" />
+      <github class="text-white" />
     </a>
     <a
       href="https://www.linkedin.com/in/axl-eduardo-guillen-luna-b63541279/"
       target="_blank"
       class="border-b-4 rounded p-1 border-blue-600 hover:bg-blue-700 hover:border-b-0 flex items-center justify-center w-10 h-10"
     >
-      <font-awesome-icon :icon="['fab', 'linkedin']" />
+      <Linkedin class="text-white"/>
     </a>
   </div>
 </template>
 
 <script setup>
 import { useI18n } from "vue-i18n";
+import github from "../../assets/iconos/svg/github.vue";
+import Linkedin from "../../assets/iconos/svg/linkedin.vue";
+import Download from "../../assets/iconos/svg/download.vue";
+
+
 const { t } = useI18n();
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animated")],
+  plugins: [require("tailwindcss-animated"), require("flowbite/plugin")],
 };
