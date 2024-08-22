795cf98e333aabaa6629b3018ba12c925e567942
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-08-21 20:22:11 -0600

    Seccion de los certificates terminada

--- src/components/certificates/carrousel.vue
+++ src/components/certificates/carrousel.vue
@@ -1,15 +1,23 @@
 <template>
-  <swiper-container effect="cards" grab-cursor="true" class="w-80 h-fit md:w-96 ">
-    <swiper-slide v-for="(certificate, index) in certificates" :key="index">
-      <img :src="`../../assets/certificates/${certificate.image}`" :alt="certificate.name" class="w-full h-auto" />
+  <swiper-container effect="cards" grab-cursor="true" class="w-80 h-fit md:w-96">
+    <swiper-slide v-for="(certificate, index) in certificates" :key="index" class="relative group">
+      <img :src="getImageSrc(certificate.image)" :alt="certificate.name" class="w-full h-auto rounded-xl"/>
+      <div class="absolute inset-0 bg-gradient-to-t from-black/90 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-xl"></div>
+      <div class="absolute bottom-0 left-0 right-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-white">
+        <p class="text-lg font-semibold">{{ certificate.name }}</p>
+        <p class="text-sm">by {{ certificate.author }}</p>
+      </div>
     </swiper-slide>
   </swiper-container>
 </template>
 
+
 <script setup>
 import {certificates} from "../../data/certificates.json"
 
-
+const getImageSrc = (imageName) => {
+  return new URL(`../../assets/certificates/${imageName}`, import.meta.url).href;
+};
 </script>
 
 <style scoped>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -18,10 +18,8 @@ const { t } = useI18n();
 
       <div class="flex flex-col md:flex-row p-3 md:p-5 lg:p-10 gap-5">
         <div class="md:mx-0 mx-auto md:w-2/5 xl:w-1/2">
-          <h1 class="text-2xl xl:text-4xl mb-3">Estudios adicionales</h1>
-          <p class="text-sm text-balance">
-            Qui minim do labore pariatur laboris veniam. In ea occaecat reprehenderit irure cillum id culpa sit labore amet cillum. Ut fugiat voluptate amet minim qui Lorem nostrud eiusmod voluptate commodo pariatur sint. Esse culpa nostrud occaecat nostrud. Non laboris minim mollit incididunt do qui enim in voluptate. Nulla consequat laboris commodo ad do culpa veniam laborum anim fugiat pariatur aute aliquip.
-          </p>
+          <h1 class="text-3xl xl:text-4xl mb-3">{{ t('about.subtitle') }}</h1>
+          <p class="text-sm text-balance font-light lg:text-base xl:text-lg">{{ t('about.history') }}</p>
         </div>
         <carrousel />
       </div>
@@ -46,7 +44,7 @@ const { t } = useI18n();
   height: 100%;
   background-image: url('../assets/bg/parallax.svg');
   background-attachment: fixed;
-  z-index: -1; /* Asegura que la imagen de fondo esté detrás del contenido */
+  z-index: -1;
 }
 
 .gradient-overlay {
