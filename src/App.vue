ea5c7e34da51df7b99087f61d0372fc012ecdc51
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-11 20:24:16 -0600

    Primera version del navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import Navbar from './components/shared/Navbar.vue';
+
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
 </template>
 
