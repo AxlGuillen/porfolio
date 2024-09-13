ffa6dfcc530230dc19fae34afdc6306b4b6db500
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-12 20:31:43 -0600

    Imagenes de banderas para los idiomas y sustitucion de los svgs por las imagenes

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -43,8 +43,8 @@ const changeLanguage = (lang) => {
     <div class="2xl:max-w-[1120px] lg:max-w-[860px] h-12 md:h-16 flex flex-wrap items-center justify-end md:justify-between mx-auto py-1 px-1 md:p-4 gap-x-5">
     <div class="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
         <button type="button" data-dropdown-toggle="language-dropdown-menu" class="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-white rounded-lg cursor-pointer hover:bg-neutral-700">
-            <svg v-if="currentLanguageLabel === 'English (US)'" class="w-5 h-5 rounded-full me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" stroke-width="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use xlink:href="#a" y="420"/><use xlink:href="#a" y="840"/><use xlink:href="#a" y="1260"/></g><use xlink:href="#a" y="1680"/></g><use xlink:href="#b" x="247" y="210"/></g><use xlink:href="#c" x="494"/></g><use xlink:href="#d" x="988"/><use xlink:href="#c" x="1976"/><use xlink:href="#e" x="2470"/></g></svg>
-            <svg v-if="currentLanguageLabel === 'Español'" class="h-5 w-5 rounded-full me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-it" viewBox="0 0 512 512"><g fill-rule="evenodd" stroke-width="1pt"><path fill="#fff" d="M0 0h512v512H0z"/><path fill="#009246" d="M0 0h170.7v512H0z"/><path fill="#ce2b37" d="M341.3 0H512v512H341.3z"/></g></svg>              
+            <img v-if="currentLanguageLabel === 'English (US)'" class="w-6 me-2" src="../../assets/iconos/flagsIcons/banderaEUA.png" alt="Bandera de EUA">          
+            <img v-if="currentLanguageLabel === 'Español'" class="w-6 me-2" src="../../assets/iconos/flagsIcons/banderaMexico.png" alt="Bandera de Mexicana">          
             {{ currentLanguageLabel }}
         </button>
         <!-- Dropdown -->
@@ -53,7 +53,7 @@ const changeLanguage = (lang) => {
                 <li @click="changeLanguage('en')">
                     <a href="#" class="block px-4 py-2 text-sm text-gray-400 hover:bg-neutral-600 hover:text-white" role="menuitem">
                     <div class="inline-flex items-center">
-                        <svg class="w-5 h-5 rounded-full me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 3900 3900"><path fill="#b22234" d="M0 0h7410v3900H0z"/><path d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0" stroke="#fff" stroke-width="300"/><path fill="#3c3b6e" d="M0 0h2964v2100H0z"/><g fill="#fff"><g id="d"><g id="c"><g id="e"><g id="b"><path id="a" d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"/><use xlink:href="#a" y="420"/><use xlink:href="#a" y="840"/><use xlink:href="#a" y="1260"/></g><use xlink:href="#a" y="1680"/></g><use xlink:href="#b" x="247" y="210"/></g><use xlink:href="#c" x="494"/></g><use xlink:href="#d" x="988"/><use xlink:href="#c" x="1976"/><use xlink:href="#e" x="2470"/></g></svg>
+                        <img class="w-6 me-2" src="../../assets/iconos/flagsIcons/banderaEUA.png" alt="Bandera de EUA">          
                         English (US)
                     </div>
                     </a>
@@ -61,7 +61,7 @@ const changeLanguage = (lang) => {
                 <li @click="changeLanguage('es')">
                     <a href="#" class="block px-4 py-2 text-sm text-gray-400 hover:bg-neutral-600 hover:text-white" role="menuitem">
                     <div class="inline-flex items-center">
-                        <svg class="h-5 w-5 rounded-full me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" id="flag-icon-css-it" viewBox="0 0 512 512"><g fill-rule="evenodd" stroke-width="1pt"><path fill="#fff" d="M0 0h512v512H0z"/><path fill="#009246" d="M0 0h170.7v512H0z"/><path fill="#ce2b37" d="M341.3 0H512v512H341.3z"/></g></svg>              
+                        <img class="w-6 me-2" src="../../assets/iconos/flagsIcons/banderaMexico.png" alt="Bandera de Mexicana">          
                         Español
                     </div>
                     </a>
