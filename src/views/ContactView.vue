baf514be3f3d92b4fbfd86468bd68e5d130d4a15
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-13 19:37:20 -0600

    Titulo en la seccion de contacto

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,17 +1,19 @@
 <script setup>
+import { useI18n } from "vue-i18n";
+import Title from "../components/shared/TitleComponent.vue";
+
+const { t } = useI18n();
+
+
 </script>
 
 <template>
   <div
-    class="template bg-cover bg-no-repeat animate-fade-right animate-ease-linear"
+    class="flex flex-col gap-5 p-3 md:p-5 lg:p-10 mx-auto justify-center lg:max-w-4xl 2xl:max-w-6xl"
   >
-    <h1>Projects</h1>
+    <Title :title="t('contact.title')" />
   </div>
 </template>
 
 <style scoped>
-.template {
-  height: 500px;
-  background-image: url("");
-}
 </style>
