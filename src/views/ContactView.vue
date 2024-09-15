c2e564e2d58048820f2c70bdb93fef58aded0023
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-09-14 21:16:29 -0600

    Primera version del formulario

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { useI18n } from "vue-i18n";
 import Title from "../components/shared/TitleComponent.vue";
+import ContactForm from "../components/contact/contactForm.vue";
+
 
 const { t } = useI18n();
 
@@ -12,6 +14,16 @@ const { t } = useI18n();
     class="flex flex-col gap-5 p-3 md:p-5 lg:p-10 mx-auto justify-center lg:max-w-4xl 2xl:max-w-6xl"
   >
     <Title :title="t('contact.title')" />
+
+    <div class="flex flex-row justify-around">
+      <div>
+        <h2>{{t( 'contact.subtitle' )}}</h2>
+      </div>
+      <div>
+        <ContactForm />
+      </div>
+    </div>
+
   </div>
 </template>
 
