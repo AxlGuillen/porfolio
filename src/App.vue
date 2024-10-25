9ab7520c0f7c91e59a3cc151d3d135a7fcb61d34
Axl Eduardo Guillen Luna <axl1504@hotmail.com>
2024-10-24 23:51:40 -0600

    Inclusion de las alertas en la raiz del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import AlertNotification from './components/shared/AlertNotification.vue';
 </script>
 
 <template>
   <RouterView />
+  <AlertNotification />
 </template>
 
 <style scoped>
